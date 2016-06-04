a40d5dca88e8ff249e97e31df59a35d53da559ca
Mark <markvapps@gmail.com>
2016-06-05 01:31:58 +0930

    add f1 support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Botkit = require('botkit')
 var Request = require('request')
+var Moment = require('moment')
 
 var token = process.env.SLACK_TOKEN
 
@@ -91,12 +92,145 @@ controller.hears('!weather (.*)', ['direct_message', 'direct_mention', 'mention'
     if (Array.isArray(result.query.results.channel))
       channel = result.query.results.channel[0]
 
-    var returnMessage = channel.item.title + '\n`Current` ' + channel.item.condition.temp + ' degrees, ' + channel.item.condition.text + '\n`' + channel.item.forecast[0].day + '` High: ' + channel.item.forecast[0].high + ' Low: ' + channel.item.forecast[0].low + ', ' + channel.item.forecast[0].text + '\n`' + channel.item.forecast[1].day + '` High: ' + channel.item.forecast[1].high + ' Low: ' + channel.item.forecast[1].low + ', ' + channel.item.forecast[1].text;
+    var reply = channel.item.title + '\n`Current` ' + channel.item.condition.temp + ' degrees, ' + channel.item.condition.text + '\n`' + channel.item.forecast[0].day + '` High: ' + channel.item.forecast[0].high + ' Low: ' + channel.item.forecast[0].low + ', ' + channel.item.forecast[0].text + '\n`' + channel.item.forecast[1].day + '` High: ' + channel.item.forecast[1].high + ' Low: ' + channel.item.forecast[1].low + ', ' + channel.item.forecast[1].text;
 
-    return bot.reply(message, returnMessage);
+    return bot.reply(message, reply);
   })
 
 })
+
+controller.hears('!nextrace', ['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message){
+  var url = 'http://ergast.com/api/f1/current/next.json';
+
+  Request.get(url, function(error, response, body) {
+    var result = JSON.parse(body);
+    var races = result.MRData.RaceTable.Races;
+    var race = races.pop();
+
+    var raceDate = Moment(race.date + ' ' + race.time);
+    var reply = {
+      text: '',
+      attachments: [{
+        fields: [{
+          title: 'Race',
+          value: race.raceName,
+          short: true
+        },{
+          title: 'Round',
+          value: race.round,
+          short: true
+        },{
+          title: 'Circuit',
+          value: '<' + race.Circuit.url + '|' + race.Circuit.circuitName + '>',
+          short: true
+        },{
+          title: 'Date/Time',
+          value: raceDate.format('dddd, MMMM Do, h:mm a'),
+          short: true
+        }],
+        text: '',
+        thumb_url: 'https://www.formula1.com/etc/designs/fom-website/icon128x128.png',
+        footer: 'Formula 1',
+        footer_icon: 'https://www.formula1.com/etc/designs/fom-website/icon128x128.png'
+      }],
+    };
+
+    return bot.reply(message, reply);
+  })
+
+})
+
+controller.hears(['!lastrace', '!race (.*) (.*)'], ['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message){
+  var year = message.match[1];
+  var round = message.match[2];
+
+  if(!year || !round) {
+    year = 'current';
+    round = 'last';
+  }
+  var url = 'http://ergast.com/api/f1/' + year + '/' + round + '/results.json';
+
+
+  Request.get(url, function(error, response, body) {
+    var result = JSON.parse(body);
+    var races = result.MRData.RaceTable.Races;
+    var race = races.pop();
+
+    var raceDate = Moment(race.date + ' ' + race.time);
+    var results = {
+      title: 'Results (Top 10)',
+      value: '',
+    };
+
+    for (var i = 0; i < Math.min(race.Results.length, 10); i++) {
+      var result = race.Results[i];
+      results.value += result.positionText + ' - ';
+      results.value += result.Driver.givenName + ' ';
+      results.value += result.Driver.familyName + ' - ';
+      results.value += result.Driver.nationality + ' - ';
+      results.value += result.Constructor.name + ' ';
+      result.Time
+        ? results.value += result.Time.time
+        : '';
+      results.value += '\n';
+    }
+
+    var reply = {
+      text: '',
+      attachments: [{
+        fields: [{
+          title: 'Race',
+          value: race.raceName,
+          short: true
+        },{
+          title: 'Round',
+          value: race.round,
+          short: true
+        },{
+          title: 'Circuit',
+          value: '<' + race.Circuit.url + '|' + race.Circuit.circuitName + '>',
+          short: true
+        },{
+          title: 'Date/Time',
+          value: raceDate.format('dddd, MMMM Do, h:mm a, YYYY'),
+          short: true
+        },
+          results
+        ],
+        text: '',
+        thumb_url: 'https://www.formula1.com/etc/designs/fom-website/icon128x128.png',
+        footer: 'Formula 1',
+        footer_icon: 'https://www.formula1.com/etc/designs/fom-website/icon128x128.png'
+      }],
+    };
+
+    return bot.reply(message, reply);
+  })
+
+})
+
+controller.hears('another_keyword','direct_message,direct_mention',function(bot,message) {
+  var reply_with_attachments = {
+    'username': 'My bot' ,
+    'text': 'This is a pre-text',
+    'attachments': [
+      {
+        'fallback': 'To be useful, I need you to invite me in a channel.',
+        'title': 'How can I help you?',
+        'text': 'To be useful, I need you to invite me in a channel ',
+        'color': '#7CD197'
+      },{
+        'fallback': 'To be useful, I need you to invite me in a channel.',
+        'title': 'How can I help you?',
+        'text': 'To be useful, I need you to invite me in a channel ',
+        'color': '#7CD197'
+      }
+    ],
+    'icon_url': 'http://lorempixel.com/48/48'
+    }
+
+  bot.reply(message, reply_with_attachments);
+});
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
