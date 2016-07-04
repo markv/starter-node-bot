3ddbcc84c8d487d4c6761071b2254bba2da902da
Mark <markvapps@gmail.com>
2016-07-04 17:12:32 +0930

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var token = process.env.SLACK_TOKEN
 var controller = Botkit.slackbot({
   // reconnect to Slack RTM when connection goes bad
   retry: Infinity,
-  debug: 7
+  debug: false
 })
 
 // Assume single team mode if we have a SLACK_TOKEN
